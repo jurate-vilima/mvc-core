23916da1a1dddf219d5a494c81139ac5926153a4
Jurate Vilima <jurate.vilima@gmail.com>
2024-11-13 18:29:20 +0200

    Added event listeners BEFORE and AFTER

--- Application.php
+++ Application.php
@@ -11,6 +11,9 @@ class Application {
     public Session $session;
     public static string $ROOT_DIR;
     public static string $BASE_URL;
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+    private array $eventListeners = [];
 
     public function __construct($config) {
         self::$app = $this;
@@ -27,6 +30,22 @@ class Application {
     }
 
     public function run() {
+        $this->trigger(self::EVENT_BEFORE_REQUEST);
         echo $this->router->resolve();
+    }
+
+    public function on($event, $callback) {
+        if(!isset($this->eventListeners[$event])) 
+            $this->eventListeners[$event] = [];
+
+        array_push($this->eventListeners[$event], $callback);
+    }
+
+    public function trigger($event) {
+        if(isset($this->eventListeners[$event])) {
+            foreach($this->eventListeners[$event] as $event) {
+                call_user_func($event);
+            }
+        }
     }
 }
